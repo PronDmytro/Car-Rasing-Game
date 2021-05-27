1d416835caed569e0103942d88f53d16acb5f483
PronDmytro <dmytropron5838@gmail.com>
2021-05-27 18:30:40 +0300

    add checking internet connection

--- NFS/StartForm.cs
+++ NFS/StartForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.NetworkInformation;
 using System.Windows.Forms;
 using NLog;
 
@@ -25,26 +26,57 @@ namespace NFS
             if (progressBar1.Value == 100)
             {
                 timer1.Stop();
-                logger.Info("Loading is successful!");
-                label1.Text = @"Successful";
-                startButton.Enabled = true;
+               
+                IPStatus status = IPStatus.TimedOut;
+                try
+                {
+                    Ping ping = new Ping();
+                    PingReply reply = ping.Send(@"google.com");
+                    status = reply.Status;
+                }
+                catch { }
+                if (status != IPStatus.Success)
+                {
+                    logger.Warn("No internet connection available");
+                    label1.Text = @"No internet connection available";
+                    MessageBox.Show(@"Check your internet connection!", @"Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    progressBar1.Value = 0;
+                    startButton.Enabled = true;
+                    startButton.Text = @"Refresh";
+                }
+                else
+                {
+                    startButton.Enabled = true;
+                    logger.Info("Loading is successful!");
+                    label1.Text = @"Successful";
+                }
             }
             else
             {
                 progressBar1.Value += 1;
             }
-            
         }
 
         private void StartButton_Click(object sender, EventArgs e)
         {
             logger.Info("Start btn is click;");
+            if (startButton.Text== @"Refresh")
+            {
+                timer1.Start();
+                label1.Text = @"Loading...";
+                startButton.Text = @"Start";
+                startButton.Enabled = false;
+            }
+            else
+            {
+                LoginForm loginForm = new LoginForm();
+                
+                loginForm.Show();
+                this.Hide();
+                logger.Info("LoginForm is open;\n");
+            }
             
-            LoginForm loginForm = new LoginForm();
-            
-            loginForm.Show();
-            this.Hide();
-            logger.Info("LoginForm is open;\n");
         }
     }
 }
